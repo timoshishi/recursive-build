314f31406c82e8df2bc8908238a853378e7483de
Tim Forrest <tim@timforrest.dev>
2024-02-21 10:42:37 -0800

    update graphql entrypoint, vercel local build deps

--- graphql.config.js
+++ graphql.config.js
@@ -1,11 +1,5 @@
-// This file is used by the VSCode GraphQL extension
+const { getPaths } = require('@redwoodjs/internal')
 
-const { getPaths } = require('@redwoodjs/project-config')
-
-/** @type {import('graphql-config').IGraphQLConfig} */
-const config = {
+module.exports = {
   schema: getPaths().generated.schema,
-  documents: './web/src/**/!(*.d).{ts,tsx,js,jsx}',
 }
-
-module.exports = config
